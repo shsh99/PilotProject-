fbfa93d038a1d1c856b9c199a2c3f98c5e61381c
신동구 <ggg9905@naver.com>
2023-11-01 17:53:24 +0900

    01 stylish 부트스트랩 테마 리액트+스프링부트+오라클 연동 예제

---
+++ 01_Blog/01_JPA/01_easy_thema/01_stylish-portfolio/frontend-typescript/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './components/common/NotFound';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/* home 페이지 */}
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
